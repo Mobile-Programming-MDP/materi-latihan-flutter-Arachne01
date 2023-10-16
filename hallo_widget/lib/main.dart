793bcaa0bc406a14f82ab7e1e708a66955174d69
Arachne01 <stephen.wijaya00@gmail.com>
2023-10-16 18:12:29 +0700

    dd

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -6,45 +6,69 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Broo",
-      theme: ThemeData(primarySwatch: Colors.lime),
-      home: FirstScreen
-    )
+        title: "Stephen W",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: firstScreen());
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen
-  ({super.key});
+class firstScreen extends StatelessWidget {
+  const firstScreen({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Broo"),
-          actions: <Widget>[IconButton(
-            onPressed: () {},
-            icon:  const Icon(
-              Icons.search,
+          title: const Text('Widget Demo'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(16),
               color: Colors.blue,
+              child: Text(
+                'Ini adalah contoh penggunaan Container',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
             ),
-          )],
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.home,
-              color: Colors.lightGreen,
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text('Elevated Button')),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-        ),
-        body: const Center(
-          child: Text("Halo Broo"),
-        ),
-      ),
-    );
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.amber),
+                Text(
+                  'Rating 4,5',
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.network(
+                'https://picsum.photos/id/7/200/300',
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ));
   }
 }
